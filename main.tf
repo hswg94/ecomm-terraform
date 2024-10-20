891069ee034c9e79e92a71aef922265a036543cf
hswg94 <hswg94@gmail.com>
2024-10-20 14:59:37 +0800

    added security group

--- main.tf
+++ main.tf
@@ -14,78 +14,78 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-# # Create VPC
-# resource "aws_vpc" "MyFypVpc" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     Name = "MyFypVpc"
-#   }
-# }
+# Create VPC
+resource "aws_vpc" "MyFypVpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyFypVpc"
+  }
+}
 
-# # Create Internet Gateway and place in the VPC
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.MyFypVpc.id
-#   tags = {
-#     Name = "MyFypVpc-igw"
-#   }
-# }
+# Create Internet Gateway and place in the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.MyFypVpc.id
+  tags = {
+    Name = "MyFypVpc-igw"
+  }
+}
 
-# # Create 2 Public Subnets
-# resource "aws_subnet" "public_subnet_1" {
-#   vpc_id                  = aws_vpc.MyFypVpc.id
-#   cidr_block              = "10.0.1.0/24"
-#   availability_zone       = "ap-southeast-1a"
-#   tags = {
-#     Name = "MyFypVpc-public-subnet-1"
-#   }
-# }
+# Create 2 Public Subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.MyFypVpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-1a"
+  tags = {
+    Name = "MyFypVpc-public-subnet-1"
+  }
+}
 
-# resource "aws_subnet" "public_subnet_2" {
-#   vpc_id                  = aws_vpc.MyFypVpc.id
-#   cidr_block              = "10.0.2.0/24"
-#   availability_zone       = "ap-southeast-1b"
-#   tags = {
-#     Name = "MyFypVpc-public-subnet-2"
-#   }
-# }
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.MyFypVpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-southeast-1b"
+  tags = {
+    Name = "MyFypVpc-public-subnet-2"
+  }
+}
 
-## Create Route Table for routing traffic to the Internet Gateway
-# resource "aws_route_table" "public_rt" {
-#   vpc_id = aws_vpc.MyFypVpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
-#   tags = {
-#     Name = "MyFypVpc-public-rt"
-#   }
-# }
+# Create Route Table for routing traffic to the Internet Gateway
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.MyFypVpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "MyFypVpc-public-rt"
+  }
+}
 
-# # Associate the route table with the subnets
-# resource "aws_route_table_association" "public_subnet_association_1" {
-#   subnet_id      = aws_subnet.public_subnet_1.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
+# Associate the route table to the subnets
+resource "aws_route_table_association" "public_subnet_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
-# resource "aws_route_table_association" "public_subnet_association_2" {
-#   subnet_id      = aws_subnet.public_subnet_2.id
-#   route_table_id = aws_route_table.public_rt.id
-# }
+resource "aws_route_table_association" "public_subnet_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
-### Create EC2 Instance
-# resource "aws_instance" "ec2-1" {
-#   ami                         = "ami-0ad522a4a529e7aa8"
-#   instance_type               = "t2.micro"
-#   iam_instance_profile        = aws_iam_instance_profile.AllowEC2AccessSM.name
-#   vpc_security_group_ids      = ["sg-04a6d76079170ea62"]
-#   subnet_id                   = "subnet-06cb493de5b519bdf"
-#   associate_public_ip_address = true
-#   user_data                   = file("./userdata.sh")
+# Create EC2 Instance
+resource "aws_instance" "ec2-1" {
+  ami                         = "ami-0ad522a4a529e7aa8"
+  instance_type               = "t2.micro"
+  iam_instance_profile        = aws_iam_instance_profile.AllowEC2AccessSM.id
+  vpc_security_group_ids      = [aws_security_group.ec2-sg.id]
+  subnet_id                   = aws_subnet.public_subnet_1.id
+  associate_public_ip_address = true
+  user_data                   = file("./userdata.sh")
 
-#   tags = {
-#     Name = "ecomm-express-api"
-#   }
-# }
+  tags = {
+    Name = "ecomm-express-api"
+  }
+}
 
 # resource "aws_instance" "ec2_2" {
 #   ami                    = var.ami
@@ -97,3 +97,5 @@ terraform {
 #     Name = "EC2Instance2"
 #   }
 # }
+
+
