d778edcb1ff8bd22ae04bbe20bd025b30da0a533
hswg94 <hswg94@gmail.com>
2024-10-20 14:59:24 +0800

    renamed files

---
+++ security-groups.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "ec2-sg" {
+  name        = "ec2-sg"
+  description = "Allow SSH, Custom TCP on port 5000, and ICMP traffic"
+  vpc_id      = aws_vpc.MyFypVpc.id
+
+  # Inbound rules
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Custom TCP on port 5000 from anywhere"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow ICMP traffic from anywhere"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rules (allow all by default)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
