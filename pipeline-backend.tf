f5d129b32560e73e164508ea90b58ce7169dd4f3
hswg94 <hswg94@gmail.com>
2024-10-27 01:26:48 +0800

    Updated Comments

--- pipeline-backend.tf
+++ pipeline-backend.tf
@@ -17,7 +17,7 @@ resource "aws_codedeploy_deployment_group" "ecomm-api-dg" {
 ////////////////////////////////////////////////////////////////////
 // This section creates the pipeline //
 
-//create connection to github (Authentication with the connection provider MUST BE completed in the AWS Console)
+//Create a connection (Authentication with GitHub MUST BE completed in the AWS Console)
 # resource "aws_codestarconnections_connection" "ecomm-api-pl-conn" {
 #   name          = "ecomm-api-pl-conn"
 #   provider_type = "GitHub"
