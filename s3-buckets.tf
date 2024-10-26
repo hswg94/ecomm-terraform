c704ae45ef5f248d09e68fa978b421f4fc01597a
hswg94 <hswg94@gmail.com>
2024-10-27 00:46:39 +0800

    added force_destroy ="true" to allow deletion of bucket without removing files

--- s3-buckets.tf
+++ s3-buckets.tf
@@ -1,6 +1,7 @@
 # This bucket is used for codepipeline to store artifacts
 resource "aws_s3_bucket" "ecomm-api-bucket" {
   bucket = "ecomm-api-storage-for-codepipeline"
+  force_destroy = "true" //allow bucket to be deleted by terraform without removing files
 }
 
 resource "aws_s3_bucket_public_access_block" "ecomm-api-bucket_pab" {
