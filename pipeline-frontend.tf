c9140f2430e005bc77bcdb3d304430a429bd98fc
hswg94 <hswg94@gmail.com>
2024-10-27 21:01:48 +0800

    established a baseline for codebuild

---
+++ pipeline-frontend.tf
@@ -0,0 +1,44 @@
+# Create the CodeBuild Source Credential
+resource "aws_codebuild_source_credential" "codebuild-credentials" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = ""
+}
+
+resource "aws_codebuild_webhook" "ecomm-frontend-builder-webhook" {
+  project_name = aws_codebuild_project.ecomm-frontend-builder.name
+  build_type   = "BUILD"
+}
+
+resource "aws_codebuild_project" "ecomm-frontend-builder" {
+  name         = "ecomm-frontend-builder"
+  description  = "This builds the ecomm-frontend and place it in S3 bucket"
+  service_role = aws_iam_role.CodeBuildRole.arn
+
+  source {
+    type      = "GITHUB"
+    location  = "https://github.com/hswg94/ecomm-react-frontend"
+    buildspec = file("buildspec.yaml")
+  }
+
+  environment {
+    type                        = "LINUX_LAMBDA_CONTAINER"
+    compute_type                = "BUILD_LAMBDA_4GB"
+    image_pull_credentials_type = "CODEBUILD"
+    image                       = "aws/codebuild/amazonlinux-x86_64-lambda-standard:nodejs20"
+  }
+
+  artifacts {
+    type                = "S3"
+    location            = aws_s3_bucket.ecomm-frontend-s3-for-cb-and-cf.bucket
+    name                = "/"
+    encryption_disabled = true
+    packaging           = "NONE"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+        status = "ENABLED"
+    }
+  }
+}
