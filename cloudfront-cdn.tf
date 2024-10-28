d809c470034e6353f26a4367b892fa06092bbdf8
hswg94 <hswg94@gmail.com>
2024-10-29 01:20:33 +0800

    added cloudfront distribution to serve static page

--- cloudfront-cdn.tf
+++ cloudfront-cdn.tf
@@ -1,118 +1,49 @@
-# resource "aws_s3_bucket" "b" {
-#   bucket = "mybucket"
+resource "aws_cloudfront_origin_access_control" "oac-for-s3" {
+  name                              = "oac-for-s3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
 
-#   tags = {
-#     Name = "My bucket"
-#   }
-# }
+resource "aws_cloudfront_distribution" "s3-distribution" {
+  enabled = true
+  origin {
+    domain_name              = aws_s3_bucket.ecomm-frontend-s3-for-cb-and-cf.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac-for-s3.id
+    origin_id                = aws_s3_bucket.ecomm-frontend-s3-for-cb-and-cf.id
+  }
+  default_cache_behavior {
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = aws_s3_bucket.ecomm-frontend-s3-for-cb-and-cf.id
 
-# resource "aws_s3_bucket_acl" "b_acl" {
-#   bucket = aws_s3_bucket.b.id
-#   acl    = "private"
-# }
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
 
-# locals {
-#   s3_origin_id = "myS3Origin"
-# }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = [] # ["US", "CA", "GB", "DE"]
+    }
+  }
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
-#     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-#     origin_id                = local.s3_origin_id
-#   }
+  price_class = "PriceClass_All"
 
-#   enabled             = true
-#   is_ipv6_enabled     = true
-#   comment             = "Some comment"
-#   default_root_object = "index.html"
-
-#   logging_config {
-#     include_cookies = false
-#     bucket          = "mylogs.s3.amazonaws.com"
-#     prefix          = "myprefix"
-#   }
-
-#   aliases = ["mysite.example.com", "yoursite.example.com"]
-
-#   default_cache_behavior {
-#     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = false
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     viewer_protocol_policy = "allow-all"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
-
-#   # Cache behavior with precedence 0
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/immutable/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = false
-#       headers      = ["Origin"]
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     min_ttl                = 0
-#     default_ttl            = 86400
-#     max_ttl                = 31536000
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-
-#   # Cache behavior with precedence 1
-#   ordered_cache_behavior {
-#     path_pattern     = "/content/*"
-#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = local.s3_origin_id
-
-#     forwarded_values {
-#       query_string = false
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#     compress               = true
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-
-#   price_class = "PriceClass_200"
-
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#       #   locations        = ["US", "CA", "GB", "DE"]
-#     }
-#   }
-
-#   tags = {
-#     Environment = "production"
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-# }
+  aliases = ["ecomm.hswg94.com"]
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.us-east-1-cert.arn
+    ssl_support_method       = "sni-only"     # sni-only for cost-effectiveness
+    minimum_protocol_version = "TLSv1.2_2021" # Ensures TLS 1.2+ for modern security compliance
+  }
+  http_version        = "http2"
+  default_root_object = "index.html"
+  is_ipv6_enabled     = true
+  depends_on = [aws_acm_certificate_validation.us-east-1-cert]
+}
