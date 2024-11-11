5b57c59830df325c1958ea9d19e30d8cf24a55a6
hswg94 <hswg94@gmail.com>
2024-11-11 22:42:44 +0800

    updated whitespaces

--- cloudfront-cdn.tf
+++ cloudfront-cdn.tf
@@ -7,18 +7,19 @@ resource "aws_cloudfront_origin_access_control" "oac-for-s3" {
 
 resource "aws_cloudfront_distribution" "s3-distribution" {
   enabled = true
+
   origin {
     domain_name              = aws_s3_bucket.ecomm-frontend-s3-for-cb-and-cf.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.oac-for-s3.id
     origin_id                = aws_s3_bucket.ecomm-frontend-s3-for-cb-and-cf.id
   }
+
   default_cache_behavior {
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
     allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = aws_s3_bucket.ecomm-frontend-s3-for-cb-and-cf.id
-
     forwarded_values {
       query_string = false
       cookies {
@@ -37,16 +38,18 @@ resource "aws_cloudfront_distribution" "s3-distribution" {
   price_class = "PriceClass_All"
 
   aliases = ["ecomm.hswg94.com"]
+
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.us-east-1-cert.arn
     ssl_support_method       = "sni-only"     # sni-only for cost-effectiveness
     minimum_protocol_version = "TLSv1.2_2021" # Ensures TLS 1.2+ for modern security compliance
   }
+
   http_version        = "http2"
   default_root_object = "index.html"
   is_ipv6_enabled     = true
   depends_on = [aws_acm_certificate_validation.us-east-1-cert]
-  
+
   custom_error_response {
     error_caching_min_ttl = 300
     error_code = 403
