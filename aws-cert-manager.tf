a14c083fe69efeaf9a13cecdf8c7e127b9345899
hswg94 <hswg94@gmail.com>
2025-02-04 17:59:11 +0800

    added more domains for www. support

--- aws-cert-manager.tf
+++ aws-cert-manager.tf
@@ -1,7 +1,11 @@
 // Certificate for resources in AP-SOUTHEAST-1
 resource "aws_acm_certificate" "ap-southeast-1-cert" {
-  domain_name       = "hswg94.com"
-  subject_alternative_names = ["*.hswg94.com"]
+  domain_name = "hswg94.com"
+  subject_alternative_names = [
+    "*.hswg94.com",
+    "*.api.hswg94.com",
+    "*.ecomm.hswg94.com"
+  ]
   validation_method = "DNS"
 }
 
@@ -14,14 +18,14 @@ resource "aws_acm_certificate_validation" "ap-southeast-1-cert" {
 
 // Certificate for CloudFront in US-EAST-1
 resource "aws_acm_certificate" "us-east-1-cert" {
-  provider = aws.us-east-1
-  domain_name       = "hswg94.com"
+  provider                  = aws.us-east-1
+  domain_name               = "hswg94.com"
   subject_alternative_names = ["*.hswg94.com"]
-  validation_method = "DNS"
+  validation_method         = "DNS"
 }
 
 resource "aws_acm_certificate_validation" "us-east-1-cert" {
-  provider = aws.us-east-1
+  provider                = aws.us-east-1
   certificate_arn         = aws_acm_certificate.us-east-1-cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert-validation : record.fqdn]
 }
