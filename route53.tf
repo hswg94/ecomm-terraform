f0cc46e11838b3a7e3e401c2a4a38a7aceb1d2d5
hswg94 <hswg94@gmail.com>
2025-02-04 16:04:59 +0800

    Created Records for www.

--- route53.tf
+++ route53.tf
@@ -17,19 +17,7 @@ resource "aws_route53domains_registered_domain" "update-domain-ns" {
   }
 }
 
-//Create Records
-resource "aws_route53_record" "api-backend-endpoint" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "api.hswg94.com"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.ecomm-api-alb.dns_name
-    zone_id                = aws_lb.ecomm-api-alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
+// Create Record for Validating Certificate
 resource "aws_route53_record" "cert-validation" {
   for_each = {
     for dvo in aws_acm_certificate.ap-southeast-1-cert.domain_validation_options : dvo.domain_name => {
@@ -46,16 +34,43 @@ resource "aws_route53_record" "cert-validation" {
   zone_id         = aws_route53_zone.primary.zone_id
 }
 
+//Create Records for API
+resource "aws_route53_record" "api-backend-endpoint" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "api.hswg94.com"
+  type    = "A"
 
-//Create Records
+  alias {
+    name                   = aws_lb.ecomm-api-alb.dns_name
+    zone_id                = aws_lb.ecomm-api-alb.zone_id
+    evaluate_target_health = true
+  }
+}
+resource "aws_route53_record" "api-backend-endpoint_cname" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.api.hswg94.com"
+  type    = "CNAME"
+  ttl = 300
+  records = [aws_route53_record.api-backend-endpoint.name]
+}
+
+
+//Create Records for Frontend CloudFront Distribution
 resource "aws_route53_record" "frontend-endpoint" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "ecomm.hswg94.com"
   type    = "A"
-
   alias {
     name                   = aws_cloudfront_distribution.s3-distribution.domain_name
     zone_id                = aws_cloudfront_distribution.s3-distribution.hosted_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "frontend-endpoint_cname" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.ecomm.hswg94.com"
+  type    = "CNAME"
+  ttl = 300
+  records = [aws_route53_record.frontend-endpoint.name]
 }
