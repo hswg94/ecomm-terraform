e82484d9427ee1fb9925511c8f725d08e12478b0
hswg94 <hswg94@gmail.com>
2025-02-04 17:59:38 +0800

    added more domains for www. support

--- aws-cert-manager.tf
+++ aws-cert-manager.tf
@@ -20,7 +20,11 @@ resource "aws_acm_certificate_validation" "ap-southeast-1-cert" {
 resource "aws_acm_certificate" "us-east-1-cert" {
   provider                  = aws.us-east-1
   domain_name               = "hswg94.com"
-  subject_alternative_names = ["*.hswg94.com"]
+    subject_alternative_names = [
+    "*.hswg94.com",
+    "*.api.hswg94.com",
+    "*.ecomm.hswg94.com"
+  ]
   validation_method         = "DNS"
 }
 
